c45a5831035881db7ce00da925dc3fa8ce3e8717
BMOG <bruno_de_fleurian@yahoo.com>
2012-02-29 11:55:32 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '1fa019b2bb89f0ff1ffa8f0e2ea72b2ffd6df2d12569eb4c1ac10d14c9eac0c05af67547d9352eba8f877aa607c47e3ac7cc7c6c57e48955572f1329b3a58a74'
